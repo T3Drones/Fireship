2f406d7865af9c4d7960aef5f6368b95e75ca18d
Tim <t3drones@gmail.com>
2021-05-02 19:41:19 -0700

    added report provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:fireship/services/auth.dart';
+import 'package:fireship/services/globals.dart';
+import 'package:fireship/services/models.dart';
 import 'package:flutter/material.dart';
+import 'package:fireship/screens/screens.dart';
 import 'package:firebase_analytics/observer.dart';
 import 'package:firebase_analytics/firebase_analytics.dart';
-import 'package:fireship/screens/screens.dart';
 import 'package:provider/provider.dart';
 import 'package:firebase_core/firebase_core.dart';
 
@@ -17,7 +19,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
-      providers: [StreamProvider<User>.value(value: AuthService().user)],
+      providers: [
+        StreamProvider<Report>.value(value: Global.reportRef.documentStream),
+        StreamProvider<User>.value(value: AuthService().user),
+      ],
       child: MaterialApp(
         // Firebase Analytics
         navigatorObservers: [
@@ -33,8 +38,20 @@ class MyApp extends StatelessWidget {
 
         // Theme
         theme: ThemeData(
+          fontFamily: 'Nunito',
+          bottomAppBarTheme: BottomAppBarTheme(
+            color: Colors.black87,
+          ),
           // your customizations here
           brightness: Brightness.dark,
+          textTheme: TextTheme(
+            body1: TextStyle(fontSize: 18),
+            body2: TextStyle(fontSize: 16),
+            button: TextStyle(letterSpacing: 1.5, fontWeight: FontWeight.bold),
+            headline: TextStyle(fontWeight: FontWeight.bold),
+            subhead: TextStyle(color: Colors.grey),
+          ),
+          buttonTheme: ButtonThemeData(),
         ),
       ),
     );
